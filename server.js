eaf5604b84cb6e1cda34a5ce8ca045053e5fa713
copyninja277 <150160262+copyninja277@users.noreply.github.com>
2025-06-30 15:46:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const connectdB = require("./config/dbConnection")
 const dotenv= require("dotenv").config();
 const errorHandler = require("./middleware/errorHandler");
+const cors = require('cors');
+
+
 
 connectdB();
 const app = express();
 
 const port = process.env.PORT || 5000;
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
