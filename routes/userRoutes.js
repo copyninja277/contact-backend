3193f06cbe8220e0e4ed289876043e6f5d2cbb52
copyninja277 <150160262+copyninja277@users.noreply.github.com>
2025-06-30 17:17:33 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const ValidateToken = require("../middleware/validtokenhandler");
 const router = express.Router();
-const {registerUser,LoginUser,currentUser} = require("../controllers/userControllers");
+const {registerUser,LoginUser,currentUser, Otpchecker} = require("../controllers/userControllers");
 
 router.post("/register",registerUser);
 
 
 router.post("/login",LoginUser);
+router.post("/send-otp", Otpchecker);
 
 
 router.get("/current", ValidateToken, currentUser);
