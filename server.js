7154b8ec3bc4edf937aee7dd1f078000a0d63258
copyninja277 <150160262+copyninja277@users.noreply.github.com>
2025-12-02 22:48:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ connectdB();
 const app = express();
 
 const port = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors({
+  origin: "https://helpful-pie-f90657.netlify.app",
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true
+}));
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
